ded49950d6f2833214c81032467df5bf19e8f371
David Gilmar <dgproducoes55@gmail.com>
2022-03-08 22:50:16 -0400

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import NavBar from './components/NavBar';
-import './index.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/NavBar";
 
 function App() {
   return (
-<NavBar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
